4888d4ccf47af33af9f7b70da14a2f79e8b4211e
Jackson Chen <26681591+sfc9982@users.noreply.github.com>
2023-09-06 08:26:55 +0800

    Update main.py

--- Pwn/Maze/main.py
+++ Pwn/Maze/main.py
@@ -9,6 +9,7 @@ maze_size = 17276
 goal_x = 0xb2
 goal_y = 0x5d
 
+
 def err(s): sys.stderr.write("[-] %s\n" % s)
 
 
@@ -43,10 +44,11 @@ for pos in arr:
 
 im.resize((w * 4, h * 4)).save('maze.png')
 
-NONE = 0 # Not visited yet
-WALL = 1 # This position contains a wall
-GOAL = 2 # This is the position we are searching for
-DONE = 3 # Used to mark positions that have already been visited
+NONE = 0  # Not visited yet
+WALL = 1  # This position contains a wall
+GOAL = 2  # This is the position we are searching for
+DONE = 3  # Used to mark positions that have already been visited
+
 
 # Simple recursive algorithm to solve the maze. Does not find an optimal
 # path, but good enough. Does not include sub-paths leading to dead ends,
@@ -57,25 +59,26 @@ def find_path(x, y):
 
     if maze[x][y] == GOAL:
         # The path is created in reverse, while backtracking
-        path = [(x,y)]
+        path = [(x, y)]
         return True
     elif maze[x][y] == WALL or maze[x][y] == DONE:
         return False
 
     maze[x][y] = DONE
 
-    if ((x < max_x and find_path(x+1, y))
-    or  (y > 0     and find_path(x, y-1))
-    or  (x > 0     and find_path(x-1, y))
-    or  (y < max_y and find_path(x, y+1))):
+    if ((x < max_x and find_path(x + 1, y))
+            or (y > 0 and find_path(x, y - 1))
+            or (x > 0 and find_path(x - 1, y))
+            or (y < max_y and find_path(x, y + 1))):
         # Inserting instead of appending, since we are backtracking
-        path.insert(0, (x,y))
+        path.insert(0, (x, y))
         return True
 
     return False
 
+
 # Create two-dimensional array for the maze data
-maze = [[NONE for y in range(max_y+1)] for x in range(max_x+1)]
+maze = [[NONE for y in range(max_y + 1)] for x in range(max_x + 1)]
 
 # Populate it with the occupied squares
 for pos in arr: maze[pos[0]][pos[1]] = 1
@@ -88,13 +91,13 @@ find_path(1, 1)
 print(path)
 
 for i in range(1, len(path)):
-    if path[i][0] > path[i-1][0]:
+    if path[i][0] > path[i - 1][0]:
         print('east')
-    elif path[i][0] < path[i-1][0]:
+    elif path[i][0] < path[i - 1][0]:
         print('west')
-    elif path[i][1] > path[i-1][1]:
+    elif path[i][1] > path[i - 1][1]:
         print('south')
-    elif path[i][1] < path[i-1][1]:
+    elif path[i][1] < path[i - 1][1]:
         print('north')
 
 sys.exit(0)
